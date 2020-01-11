70767a563f2aa3bd707e41ccbe20f1d5eefc07d5
Man Ioan <man_ioan@live.com>
2020-01-11 17:24:33 +0200

    Move js code to JS files

---
+++ OnlineAppleShoppingStore/Scripts/Cart/Cart.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $(".RemoveLink").click(function (e) {
+        e.preventDefault();
+
+        var recordToDelete = $(this).attr("data-id");
+        if (recordToDelete != '') {
+
+            $.post("/Cart/RemoveFromCart", { "id": recordToDelete },
+                function (data) {
+
+                    if (data.ItemCount == 0) {
+                        $('#row-' + data.DeleteId).fadeOut('slow');
+                    } else {
+                        $('#item-count-' + data.DeleteId).text(data.ItemCount);
+                    }
+                    $('#cart-total').text(data.CartTotal);
+                    $('#update-message').text(data.Message);
+                    $('#cart-status').text('Cart (' + data.CartCount + ')');
+                });
+        }
+    });
+})
---
+++ OnlineAppleShoppingStore/Scripts/Cart/Slides.js
@@ -0,0 +1,4 @@
+﻿$(".rslides").responsiveSlides({
+    auto: true, // Boolean: Animate automatically, true or false
+    speed: 500 // Integer: Speed of the transition, in milliseconds
+});
