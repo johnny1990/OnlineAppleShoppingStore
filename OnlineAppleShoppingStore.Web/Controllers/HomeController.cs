0589306666f49c181b1c8bc9ccc685400b01339d
Man Ioan <man_ioan@live.com>
2020-01-25 10:17:08 +0200

    Add feedback functionality

--- OnlineAppleShoppingStore.Web/Controllers/HomeController.cs
+++ OnlineAppleShoppingStore.Web/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Net;
+using OnlineAppleShoppingStore.Web.Models;
 
 namespace OnlineAppleShoppingStore.Web.Controllers
 {
@@ -24,6 +28,37 @@ namespace OnlineAppleShoppingStore.Web.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Feedback(Feedback model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("name@gmail.com")); 
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.FeedBack);
+                message.IsBodyHtml = true;
+                using (var smtp = new SmtpClient())
+                {
+                    smtp.Send(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
             return View();
         }
     }
