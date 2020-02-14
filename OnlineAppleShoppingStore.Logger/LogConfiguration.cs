93bef43ac20ea26b55664cf7b1a50e16d36882d7
Man Ioan <man_ioan@live.com>
2020-02-15 00:13:30 +0200

    Add Logger project for logs in web application!

---
+++ OnlineAppleShoppingStore.Logger/LogConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAppleShoppingStore.Logger
+{
+    public class LogConfiguration
+    {
+        public static string LogDirectory
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["LogDirectory"];
+            }
+        }
+    }
+}
