f530a089726fe424d2fc7e26d9dd5463237ce6d0
Man Ioan <man_ioan@live.com>
2020-03-21 21:34:56 +0200

    Add Dashboard&UserList for Administration module

---
+++ OnlineAppleShoppingStore.Web/Controllers/AdministrationController.cs
@@ -0,0 +1,35 @@
+﻿using OnlineAppleShoppingStore.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineAppleShoppingStore.Web.Controllers
+{
+    //[Authorize(Roles = "Administrator")]
+    public class AdministrationController : Controller
+    {
+        private readonly IUsersRepository repository;
+
+        public AdministrationController(IUsersRepository objIrepository)
+        {
+            repository = objIrepository;
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UsersList()
+        {
+            return View(repository.All.ToList());
+        }
+
+       
+
+
+    }
+}
