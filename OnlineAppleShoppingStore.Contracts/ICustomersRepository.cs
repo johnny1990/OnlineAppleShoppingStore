6c951b905138a5418806b3e7bf7c8efb2e73fc4d
Man Ioan <man_ioan@live.com>
2020-06-10 15:22:56 +0300

    Add Cutomers entity operations & register

---
+++ OnlineAppleShoppingStore.Contracts/ICustomersRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineAppleShoppingStore.Entities.Models;
+
+namespace OnlineAppleShoppingStore.Contracts
+{
+    public interface ICustomersRepository :IDisposable
+    {
+        IQueryable<Customer> All { get; }
+        Customer Find(int? id);
+        void Insert(Customer ct);
+        void Update(Customer ct);
+        void Delete(int id);
+        void Save();
+    }
+}
---
+++ OnlineAppleShoppingStore.Repositories/CustomersRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineAppleShoppingStore.Contracts;
+using OnlineAppleShoppingStore.Entities.Models;
+
+namespace OnlineAppleShoppingStore.Repositories
+{
+    public class CustomersRepository : ICustomersRepository
+    {
+        OnlineAppleShoppingStoreEntities _db;
+
+        public CustomersRepository(OnlineAppleShoppingStoreEntities db)
+        {
+            this._db = db;
+        }
+
+        public IQueryable<Customer> All
+        {
+            get { return _db.Customers; }
+        }
+
+        public void Delete(int id)
+        {
+            var cat = _db.Customers.Find(id);
+            _db.Customers.Remove(cat);
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public Customer Find(int? id)
+        {
+            Customer objCt = new Customer();
+            objCt = _db.Customers.Where(p => p.Id == id).FirstOrDefault();
+            return objCt;
+        }
+
+        public void Insert(Customer ct)
+        {
+            _db.Customers.Add(ct);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Customer ct)
+        {
+            _db.Entry(ct).State = System.Data.Entity.EntityState.Modified;
+
+        }
+    }
+}
