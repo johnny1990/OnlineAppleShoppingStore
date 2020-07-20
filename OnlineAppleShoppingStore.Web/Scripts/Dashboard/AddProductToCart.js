1b4bfaeec9e97a8ed442cdc0bab49a25f6efdfd4
Man Ioan <man_ioan@live.com>
2020-07-20 09:34:24 +0300

    Add validation on Cart & Products.

---
+++ OnlineAppleShoppingStore.Web/Scripts/Dashboard/AddProductToCart.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $(".AddToCart").click(function () {
+        var recordToAdd = $(this).attr("data-id");
+        if (recordToAdd != '') {
+            $.post("/Cart/AddToCart", { "id": recordToAdd },
+                function (data) {
+                    $('#cart-status').text(data.CartCount);
+                });
+        }
+    });
+});
