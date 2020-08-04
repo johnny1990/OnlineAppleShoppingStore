3a2ce77a9d05e73b77b63ad0406e7707437fae8f
Man Ioan <man_ioan@live.com>
2020-08-04 16:58:19 +0300

    Add scripts for ShoppingCart HomePage

---
+++ OnlineAppleShoppingStore.Web/Scripts/HomePage/LoadImages.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $('img').Lazy({
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: false,
+        onError: function (element) {
+            console.log('error loading ' + element.data('src'));
+        },
+        onFinishedAll: function () {
+            console.log('all images loaded')
+        }
+    });
+});
