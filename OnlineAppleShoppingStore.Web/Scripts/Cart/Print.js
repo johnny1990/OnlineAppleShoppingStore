e915f12301070d7292fbe4d5e1863ce1bde60e6a
Man Ioan <man_ioan@live.com>
2020-08-07 10:36:23 +0300

    Add print functionality to shopping cart.

---
+++ OnlineAppleShoppingStore.Web/Scripts/Cart/Print.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    $("#btnPrint").click(function () {
+        var contents = $("#dvContent").html();
+        var frame1 = $('<iframe />');
+        frame1[0].name = "frame1";
+        frame1.css({ "position": "absolute", "top": "-1000000px" });
+        $("body").append(frame1);
+        var frameDoc = frame1[0].contentWindow ? frame1[0].contentWindow : frame1[0].contentDocument.document ? frame1[0].contentDocument.document : frame1[0].contentDocument;
+        frameDoc.document.open();
+
+        frameDoc.document.write('<html><head><title>DIV Contents</title>');
+        frameDoc.document.write('</head><body>');
+
+        frameDoc.document.write('<link href="style.css" rel="stylesheet" type="text/css" />');
+  
+        frameDoc.document.write(contents);
+        frameDoc.document.write('</body></html>');
+        frameDoc.document.close();
+        setTimeout(function () {
+            window.frames["frame1"].focus();
+            window.frames["frame1"].print();
+            frame1.remove();
+        }, 500);
+    });
+});
