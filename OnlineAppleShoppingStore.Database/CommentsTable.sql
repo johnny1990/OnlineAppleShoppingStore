98823a3c54247ade88385e878dc46040d7dfba96
Man Ioan <man_ioan@live.com>
2020-09-29 13:35:35 +0300

    Update model

--- OnlineAppleShoppingStore.Database/CommentsTable.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Comments] (
-    [CommentId] INT            IDENTITY (1, 1) NOT NULL,
-    [Comment]   NVARCHAR (MAX) NULL,
-    [TopicId]   INT            NOT NULL,
-    CONSTRAINT [PK_dbo.Comments] PRIMARY KEY CLUSTERED ([CommentId] ASC),
-    CONSTRAINT [FK_dbo.Comments_dbo.Topics_TopicId] FOREIGN KEY ([TopicId]) REFERENCES [dbo].[Topics] ([TopicId]) ON DELETE CASCADE
-);
-
--- OnlineAppleShoppingStore.Database/TopicsTable.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Topics] (
-    [TopicId]     INT            IDENTITY (1, 1) NOT NULL,
-    [TopicName]   NVARCHAR (MAX) NOT NULL,
-    [Description] NVARCHAR (120) NOT NULL,
-    [Body]        NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_dbo.Topics] PRIMARY KEY CLUSTERED ([TopicId] ASC)
-);
-
