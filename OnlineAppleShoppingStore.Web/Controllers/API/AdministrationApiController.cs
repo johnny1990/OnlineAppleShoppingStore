4d7dfc33738b15155f7895ad0131c4acca21654f
Man Ioan <man_ioan@live.com>
2020-12-11 08:48:17 +0200

    Implement API for administration

---
+++ OnlineAppleShoppingStore.Web/Controllers/API/AdministrationApiController.cs
@@ -0,0 +1,47 @@
+﻿using OnlineAppleShoppingStoreAuth.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OnlineAppleShoppingStore.Web.Controllers.API
+{
+    public class AdministrationApiController : ApiController
+    {
+        AuthEntities db = new AuthEntities();
+
+        [HttpGet]
+        [Route("api/AdministrationApi/GetUsers")]
+        public IHttpActionResult GetUsers()
+        {
+            try
+            {
+                var result = from a in db.AspNetUsers.ToList()
+                             select new
+                             {
+                                 a.Id,
+                                 a.Email,
+                                 a.EmailConfirmed,
+                                 a.PasswordHash,
+                                 a.SecurityStamp,
+                                 a.PhoneNumber,
+                                 a.PhoneNumberConfirmed,
+                                 a.TwoFactorEnabled,
+                                 a.LockoutEndDateUtc,
+                                 a.LockoutEnabled,
+                                 a.AccessFailedCount,
+                                 a.UserName
+                             };
+                return Ok(result);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+
+    }
+}
