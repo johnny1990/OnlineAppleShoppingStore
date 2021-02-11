16676788f6deb5f92dd1e20ab9bef70f05bb3a9f
Man Ioan <man_ioan@live.com>
2021-02-11 14:38:01 +0200

    Add shipper table script

---
+++ OnlineAppleShoppingStore.Database/ShippersOrdersTable.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ShippersOrders] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [Name]  NVARCHAR (40) NOT NULL,
+    [Phone] NVARCHAR (24) NULL,
+    CONSTRAINT [PK_ShippersOrders] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
