671c45baaba2194f301e36a0fbf25aa55ad5c3c2
Man Ioan <man_ioan@live.com>
2021-03-28 21:07:05 +0300

    Implement Alert using TypeScript

---
+++ OnlineAppleShoppingStore.Web/Scripts/Scripts/TypeScript/Message.js
@@ -0,0 +1,7 @@
+function AlertUser() {
+    document.getElementById("msgDiv").innerHTML = MessageUser("Shaikh");
+}
+function MessageUser(user) {
+    return "<h2>Hello " + user + ", your order was registered!</h2>";
+}
+//# sourceMappingURL=Message.js.map
---
+++ OnlineAppleShoppingStore.Web/Scripts/TsConfig/Message.ts
@@ -0,0 +1,7 @@
+﻿function AlertUser() {
+    document.getElementById("msgDiv").innerHTML = MessageUser("Shaikh");
+}
+
+function MessageUser(user: string) {
+    return "<h2>Hello " + user + ", your order was registered!</h2>";
+}
