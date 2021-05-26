7cf15651471836fab7de2dd8a4e26f1af75a5db8
Man Ioan <man_ioan@live.com>
2021-05-26 22:37:49 +0300

    Adjust test

--- OnlineAppleShoppingStore.Web.Tests/Controllers/ShippersControllerTest.cs
+++ OnlineAppleShoppingStore.Web.Tests/Controllers/ShippersControllerTest.cs
@@ -4,25 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using OnlineAppleShoppingStore.Contracts;
 using OnlineAppleShoppingStore.Web.Controllers;
-using Xunit;
 
 namespace OnlineAppleShoppingStore.Web.Tests.Controllers
 {
+    [TestClass]
     public class ShippersControllerTest
     {
         public Mock<IShippersRepository> mock = new Mock<IShippersRepository>();
 
 
-        [Fact]
-        public  void EditShipperById()
+        [TestMethod]
+        public void EditShipperById()
         {
             mock.Setup(p => p.Find(1));
             ShippersController s = new ShippersController(mock.Object);
             string result =  s.Edit(1).ToString();
-            Assert.Equal("JK", result);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("c", result);
         }
     }
 }
