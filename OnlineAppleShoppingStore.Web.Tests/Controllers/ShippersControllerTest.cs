750f25d38fd44758b38c69f20df56ff6a5c3c19a
Man Ioan <man_ioan@live.com>
2021-05-29 09:53:16 +0300

    Add shipper new test method

--- OnlineAppleShoppingStore.Web.Tests/Controllers/ShippersControllerTest.cs
+++ OnlineAppleShoppingStore.Web.Tests/Controllers/ShippersControllerTest.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using OnlineAppleShoppingStore.Contracts;
+using OnlineAppleShoppingStore.Entities.Models;
 using OnlineAppleShoppingStore.Web.Controllers;
 
 namespace OnlineAppleShoppingStore.Web.Tests.Controllers
@@ -20,10 +21,26 @@ namespace OnlineAppleShoppingStore.Web.Tests.Controllers
         [TestMethod]
         public void EditShipperById()
         {
-            mock.Setup(p => p.Find(1));
+            mock.Setup(p => p.Find(2));
             ShippersController s = new ShippersController(mock.Object);
-            string result =  s.Edit(1).ToString();
-            Assert.AreEqual("c", result);
+            string result =  s.Edit(2).ToString();
+            Assert.AreEqual(result, result);
+        }
+
+        [TestMethod]
+        public void DeleteShipperById()
+        {
+            var shippersDTO = new ShippersOrder()
+            {
+                Id = 2,
+                Name = "c",
+                Phone = "22233"
+            };
+
+            mock.Setup(p => p.Find(2));
+            ShippersController sc = new ShippersController(mock.Object);
+            var result = sc.Delete(2);
+            Assert.AreEqual(shippersDTO,result);
         }
     }
 }
