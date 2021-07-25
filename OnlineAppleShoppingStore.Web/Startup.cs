a196f5c0d1aed423de11853073483c9c0a40783c
Man Ioan <man_ioan@live.com>
2021-07-25 16:58:36 +0300

    Add hangfire for monitorizing jobs

--- OnlineAppleShoppingStore.Web/Startup.cs
+++ OnlineAppleShoppingStore.Web/Startup.cs
@@ -1,14 +1,43 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using Hangfire;
+using Hangfire.SqlServer;
+using System.Collections.Generic;
+using System;
+using System.Diagnostics;
 
 [assembly: OwinStartupAttribute(typeof(OnlineAppleShoppingStore.Web.Startup))]
 namespace OnlineAppleShoppingStore.Web
 {
     public partial class Startup
     {
+        private IEnumerable<IDisposable> GetHangfireServers()
+        {
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage("Server=MAN; Database=HangfireTest_OnlineAppleShoppingStore; Integrated Security=True;", new SqlServerStorageOptions
+                {
+                    CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+                    SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.Zero,
+                    UseRecommendedIsolationLevel = true,
+                    DisableGlobalLocks = true
+                });
+
+            yield return new BackgroundJobServer();
+        }
+
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            app.UseHangfireAspNet(GetHangfireServers);
+            app.UseHangfireDashboard();
+
+            // Let's also create a sample background job
+            BackgroundJob.Enqueue(() => Debug.WriteLine("Hello world from Hangfire!"));
         }
     }
 }
